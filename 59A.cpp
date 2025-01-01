2fc3beca17537b98310555f957752684018e9fa3
IlyasKhan11 <ilyas.dev2021@gmail.com>
2025-01-01 22:35:13 +0500

    A.Watermelon

--- 59A.cpp
+++ 59A.cpp
@@ -1,18 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define REP(i, a, b) for (int i = a; i <= b; i++)
-#define all(v) v.begin(), v.end()
-
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> pi;
-
-
 int main() {
     string s;
     cin >> s;
