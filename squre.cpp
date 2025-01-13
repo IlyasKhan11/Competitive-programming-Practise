de9c5336c3cbb71a9dde53b075a375053848abba
IlyasKhan11 <ilyas.dev2021@gmail.com>
2025-01-13 23:35:49 +0500

    atcoder begineer contest 387

---
+++ squre.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define all(v) v.begin(), v.end()
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+int main() {
+    int l,h;
+    cin>>l>>h;
+
+    int ans=(l+h);
+    ans=ans*ans;
+
+    cout<<ans<<endl;
+
+
+
+
+    return 0;
+}
