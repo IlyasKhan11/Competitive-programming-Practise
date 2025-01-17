815f04038817e9cbadf0d8ef91411e601069d62e
IlyasKhan11 <ilyas.dev2021@gmail.com>
2025-01-17 21:27:21 +0500

    Sieve of Eratosthenes

--- number theory/divisors.cpp
+++ number theory/divisors.cpp
@@ -1,12 +1,14 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+const int N = 100000;
+
+// Count factors of a single number
 int primesN(int n) {
     int count = 0;
     for (int i = 1; i * i <= n; i++) {
         if (n % i == 0) {
             count++;
-
             if (i != n / i) {
                 count++;
             }
@@ -14,31 +16,55 @@ int primesN(int n) {
     }
 
     cout << count << endl;
-
     return count;
 }
 
+// Count factors for all numbers up to n
 void primesToN(int n) {
     vector<int> factors(n + 1, 0);
 
     for (int i = 1; i <= n; i++) {
-        for (int j = i; j <= n; j += i) { // Corrected increment to j += i
+        for (int j = i; j <= n; j += i) { // Increment by i
             factors[j]++;
         }
     }
 
-    for (int i = 1; i < factors.size(); i++) { // Start from 1 to skip 0th index
-        cout << factors[i] << endl;
+    for (int i = 1; i < factors.size(); i++) { // Start from 1
+        cout << "Factors of " << i << ": " << factors[i] << endl;
+    }
+    cout << endl;
+}
+
+#include "bits/stdc++.h"
+using namespace std;
+
+// Sieve of Eratosthenes to find all prime numbers up to n
+void SOE(int n) {
+    vector<bool> sieve(n + 1, false); 
+
+    for (int x = 2; x <= n; x++) {
+        if (sieve[x]) continue; 
+        for (int u = 2 * x; u <= n; u += x) {
+            sieve[u] = true;
+        }
+    }
+
+    // Output all primes
+    cout << "Prime numbers up to " << n << ":" << endl;
+    for (int i = 2; i <= n; i++) {
+        if (!sieve[i]) { // Unmarked numbers are primes
+            cout << i << " ";
+        }
     }
     cout << endl;
 }
 
 int main() {
     int n;
+    cout << "Enter the value of n: ";
     cin >> n;
 
-    // primesN(n); // Uncomment if you need to test the primesN function
-    primesToN(n);
+    SOE(n); 
 
     return 0;
 }
