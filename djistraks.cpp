ab7c8eebb4928174f0f55fe5884b4d2ae4888c14
IlyasKhan11 <ilyas.dev2021@gmail.com>
2025-03-01 23:36:12 +0500

    djistraks cpp

---
+++ djistraks.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pii;
+
+vector<int> dijkstra(int n, vector<vector<pii>>& adj, int src) {
+    vector<int> dist(n, INT_MAX);
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    dist[src] = 0;
+    pq.push({0, src});
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        int d = pq.top().first;
+        pq.pop();
+        if (d > dist[u]) continue;
+        for (auto& edge : adj[u]) {
+            int v = edge.first, w = edge.second;
+            if (dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+    return dist;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<pii>> adj(n);
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+    int src;
+    cin >> src;
+    vector<int> dist = dijkstra(n, adj, src);
+    for (int i = 0; i < n; i++) cout << dist[i] << " ";
+    return 0;
+}
